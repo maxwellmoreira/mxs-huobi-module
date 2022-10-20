481b5dc21b7d82211c1b31ea9cd785dc3e749e91
mxs <max.a.moreira@gmail.com>
2022-10-19 23:34:38 -0300

    Initial commit.

--- src/test/java/com/mxs/huobi/factory/FeignFactory.java
+++ src/test/java/com/mxs/huobi/factory/FeignFactory.java
@@ -1,6 +1,5 @@
 package com.mxs.huobi.factory;
 
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
